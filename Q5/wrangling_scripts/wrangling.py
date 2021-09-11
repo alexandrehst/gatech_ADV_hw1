6bf5ecbe3149264882711ee550e4ef9d8ca5c30f
Alexandre Torres <alexandrehst@gmail.com>
2021-09-11 15:14:23 -0300

    Q5

--- Q5/wrangling_scripts/wrangling.py
+++ Q5/wrangling_scripts/wrangling.py
@@ -7,14 +7,14 @@ that will be supplied to the template for generating the table. """
 import csv
 
 def username():
-    return 'gburdell3'
+    return 'atorres78'
 
 def data_wrangling():
     with open('data/movies.csv', 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         table = list()
         # Feel free to add any additional variables
-        ...
+        n_lines = 0
         
         # Read in the header
         for header in reader:
@@ -22,13 +22,13 @@ def data_wrangling():
         
         # Read in each row
         for row in reader:
+            if n_lines >= 100:
+                break
+            n_lines +=1 
             table.append(row)
             
-            # Only read first 100 data rows - [2 points] Q5.a
-            ...
-        
         # Order table by the last column - [3 points] Q5.b
-        ...
+        newTable = sorted(table, key=lambda item: float(item[2]), reverse=True)
     
-    return header, table
+    return header, newTable
 
